dc54eea61f04425f797c7083dea551bdae3336bd
Ridercogswell <ridercogswell@gmail.com>
2022-02-01 13:08:59 -0800

    add html form

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,7 +2,7 @@ var getUserRepos = function(user) {
     // format the github api url
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
-    // make a request to the url
+    // make a request to the url with nested then(function())
     fetch(apiUrl).then(function(response) {
         response.json().then(function(data) {
             console.log(data);
@@ -11,3 +11,11 @@ var getUserRepos = function(user) {
 };
 
 getUserRepos();
+
+
+
+// Add Search form
+// Handle form submission
+// Display response data on page
+// Add error handling
+// Save progress con git
