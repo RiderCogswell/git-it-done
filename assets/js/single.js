08f6a200eb72fdae9442f5c23eb85d698b655d2c
Ridercogswell <ridercogswell@gmail.com>
2022-02-02 12:42:57 -0800

    redirect

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,36 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+
+var getRepoName = function() {
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+
+    if (repoName) {
+        // display repo name on the page
+        repoNameEl.textContent = repoName;
+
+        getRepoIssues(repoName);
+    } else {
+        // if no repo was given, redirect to homepage
+        document.location.replace("./index.html");
+    }
+
+}
+
+var displayWarning = function(repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    // append to warning container 
+    limitWarningEl.appendChild(linkEl);
+}
 
 var displayIssues = function(issues) {
     if (issues.length === 0) {
@@ -34,7 +66,7 @@ var displayIssues = function(issues) {
         issueEl.appendChild(typeEl);
         
         issueContainerEl.appendChild(issueEl);
-    }
+    };
 }
 
 var getRepoIssues = function(repo) {
@@ -46,11 +78,18 @@ var getRepoIssues = function(repo) {
             response.json().then(function(data) {
                 // pass response data to dom
                 displayIssues(data);
-            })
+
+                // check if api has paginated issues
+                if (response.headers.get("Link")) {
+                    displayWarning(repo);
+                }
+            });
         } else {
-            alert("There was a problem with your request!");
+            // if not successful, redirect to homepage
+            document.location.replace("./index.html");
         }
     });
 }
 
-getRepoIssues("facebook/react")
+
+getRepoName();
